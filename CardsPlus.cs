674cf634c6e28376af6e157b4c90cc1e4304a9d9
willis81808 <willis81808@gmail.com>
2021-07-08 02:17:27 -0600

    bumped version

--- CardsPlus.cs
+++ CardsPlus.cs
@@ -14,7 +14,7 @@ using Photon.Pun;
 namespace CardsPlusPlugin
 {
     [BepInDependency("com.willis.rounds.unbound", BepInDependency.DependencyFlags.HardDependency)]
-    [BepInPlugin(ModId, ModName, "1.1.0")]
+    [BepInPlugin(ModId, ModName, "1.1.1")]
     [BepInProcess("Rounds.exe")]
     public class CardsPlus : BaseUnityPlugin
     {
