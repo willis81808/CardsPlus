2320c5276960adc65383b93bacd8feb820b77bd3
willis81808 <willis81808@gmail.com>
2022-09-29 00:33:03 -0600

    incremented version

--- CardsPlus.cs
+++ CardsPlus.cs
@@ -21,7 +21,7 @@ using CardsPlusPlugin.Utils;
 namespace CardsPlusPlugin
 {
     [BepInDependency("com.willis.rounds.unbound", BepInDependency.DependencyFlags.HardDependency)]
-    [BepInPlugin(ModId, ModName, "1.6.2")]
+    [BepInPlugin(ModId, ModName, "1.6.3")]
     [BepInProcess("Rounds.exe")]
     public class CardsPlus : BaseUnityPlugin
     {
